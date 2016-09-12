6d340e119b827ab6844bbc2f709437e34f1bc3f3
Jordan Orelli <jordanorelli@gmail.com>
2016-09-11 20:42:55 -0400

    shit got hairy, time to rename things

--- ent/entity.go
+++ ent/entity.go
@@ -20,5 +20,14 @@ func (e *Entity) Read(br bit.Reader) error {
 	if err := fp.read(br, htree, e.Class); err != nil {
 		return fmt.Errorf("unable to read entity: %v", err)
 	}
+	for i := 0; i <= fp.hlast; i++ {
+		if fp.history[i][0] == 0 {
+			Debug.Printf("direct selection: %v", fp.history[i][1])
+			Debug.Printf("field: %v", e.Class.Fields[fp.history[i][1]])
+		} else {
+			Debug.Printf("child selection: %v (%v)", fp.history[i],
+				fp.history[i][1:fp.history[i][0]+2])
+		}
+	}
 	return nil
 }
